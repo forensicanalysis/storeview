d5f01c73815995cc351a59bbffab1d0b1f3df3f4
Jonas Plum <git@cugu.eu>
2020-05-19 12:32:08 +0200

    Update forensicstore

--- cobraserver/util.go
+++ cobraserver/util.go
@@ -2,15 +2,44 @@ package cobraserver
 
 import (
 	"encoding/json"
+	"github.com/forensicanalysis/forensicstore"
 	"io"
 )
 
-func PrintJSON(w io.Writer, i interface{}) error {
+func PrintAny(w io.Writer, i interface{}) error {
 	b, err := json.Marshal(i)
 	if err != nil {
 		return err
 	}
-	_, err = w.Write(b)
+
+	return PrintJSON(w, b)
+}
+
+func PrintJSONList(w io.Writer, elements []forensicstore.JSONElement) error {
+	_, err := w.Write([]byte("["))
+	if err != nil {
+		return err
+	}
+
+	for i, element := range elements {
+		if i != 0 {
+			_, err := w.Write([]byte(","))
+			if err != nil {
+				return err
+			}
+		}
+		_, err := w.Write(element)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = w.Write([]byte("]\n"))
+	return err
+}
+
+func PrintJSON(w io.Writer, b []byte) error {
+	_, err := w.Write(b)
 	if err != nil {
 		return err
 	}
